9afcf94930394bbb5d4014f415890cdd35132152
rafaeltarrendo41 <a89162@alunos.uminho.pt>
2020-03-30 20:36:37 +0100

    Criar relatória

--- 12-03GitHub/app/src/main/java/android/example/dai2/fazer_documentos.java
+++ 12-03GitHub/app/src/main/java/android/example/dai2/fazer_documentos.java
@@ -1,12 +1,15 @@
 package android.example.dai2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,39 +20,101 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class fazer_documentos extends AppCompatActivity {
-    private  EditText identificacao, relatorioo;
+    private EditText identificacao, relatorioo;
+    private String t_identific, t_relatorio;
     private boolean sucess = false, prenchido = true;
+    Button regRelat;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fazer_documentos);
         identificacao = (EditText) findViewById(R.id.identificacao);
         relatorioo = (EditText) findViewById(R.id.relatorio);
-
+        regRelat = (Button) findViewById(R.id.button7);
+        regRelat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
     }
-
-    public void btnCriarRelatorio(View view){
-        CriarRelatorio criarRelatorio = new CriarRelatorio();
-        criarRelatorio.execute();
-        if (sucess == true && prenchido == true){
-            Toast.makeText(getApplicationContext(), "Relatório Inserido com Sucesso!", Toast.LENGTH_LONG).show();
-            startActivity(new Intent(this, android.example.dai2.Main2Activity.class));
+    public void register(){
+        intialize();
+        if (!validate()){
+            Toast.makeText(this, "Campos em falta!", Toast.LENGTH_SHORT).show();
         } else {
-           // if (prenchido == false) {
-                Toast.makeText(getApplicationContext(), "É necessário preencher todos os campos!", Toast.LENGTH_LONG).show();
-           /* } else {
-                Toast.makeText(getApplicationContext(), "ERRO", Toast.LENGTH_LONG).show();
-                startActivity(new Intent(this, android.example.dai2.fazer_documentos.class));
-            }*/
+            CriarRelatorio criarRelatorio = new CriarRelatorio();
+            criarRelatorio.execute();
+            if (sucess = true) {
+                Toast.makeText(this, "Relatório criado com sucesso!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, android.example.dai2.Main2Activity.class));
+            } else {
+                Toast.makeText(this, "ERRO", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+   /* public void onRegisterRelat(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(BD.getBdUrl(), BD.getUSER(), BD.getPASS());
+            String msg;
+            int valor=0;
+            if (connection == null){
+                sucess = false;
+                msg = "Não foi possível realizar connection";
+            } else {
+                //if (prenchido == true) {
+                String query1 = "SELECT COUNT(1) FROM Recluse WHERE id_recluse like '" + t_identific + "';";
+                Statement statement1 = connection.createStatement();
+                ResultSet resultSet1 = statement1.executeQuery(query1);
+                while (resultSet1.next()) {
+                    valor = resultSet1.getInt("COUNT(1)");
+                }
+                if (valor > 0 && valor < 2) {
+                    String query = "INSERT INTO Report (`report`, `scan`, `id_recluse`) VALUES ('" + t_relatorio + "', '" + MainActivity.scanValor + "', '" + t_identific + "');";
+                    Statement statement = connection.createStatement();
+                    statement.executeUpdate(query);
+                    msg = "Inserido com sucesso";
+                    sucess = true;
+                    // prenchido = true;
+                    System.out.println(sucess);
+                    //  System.out.println(prenchido);
+                } else {
+                    Toast.makeText(this, "IDentifiacação nao encontrada!", Toast.LENGTH_SHORT).show();
+                    sucess = false;
+                    msg = "Identificação de Recluso inválida!";
+                }
+                //   }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
+    }*/
+    public  boolean validate(){
+        boolean valid = true;
+        if (t_identific.isEmpty()){
+            identificacao.setError("Introduz a identificação");
+            valid = false;
+        }
+        if (t_relatorio.isEmpty()){
+            relatorioo.setError("Introduza um relatorio");
+            valid = false;
+        }
+        return valid;
+    }
+    public void intialize(){
+        t_identific = identificacao.getText().toString().trim();
+        t_relatorio = relatorioo.getText().toString().trim();
     }
     private class CriarRelatorio extends AsyncTask<String, String, String>{
         String msg = "";
         String scan = MainActivity.scanValor;
-        String identificacao1 = identificacao.getText().toString();
-        String relatorio = relatorioo.getText().toString();
+        //String identificacao1 = identificacao.getText().toString();
+        // String relatorio = relatorioo.getText().toString();
         int valor;
 
 
@@ -57,49 +122,38 @@ public class fazer_documentos extends AppCompatActivity {
         @Override
         protected String doInBackground(String... strings) {
             try {
-                if (identificacao1.equals("") || relatorio.equals("")) {
-                    prenchido = false;
-                }
-
-                System.out.println(relatorio);
                 Class.forName("com.mysql.jdbc.Driver");
                 Connection connection = DriverManager.getConnection(BD.getBdUrl(), BD.getUSER(), BD.getPASS());
                 if (connection == null){
                     sucess = false;
                     msg = "Não foi possível realizar connection";
                 } else {
-                    if (prenchido == true) {
-                        String query1 = "SELECT COUNT(1) FROM Recluse WHERE id_recluse like '" + identificacao1 + "';";
-                        Statement statement1 = connection.createStatement();
-                        ResultSet resultSet1 = statement1.executeQuery(query1);
-                        System.out.println("ficou aqui");
-                        while (resultSet1.next()) {
-                            valor = resultSet1.getInt("COUNT(1)");
-                            System.out.println(valor);
-                        }
-                        if (valor > 0 && valor < 2) {
-                            System.out.println("ainda nao");
-                            String query = "INSERT INTO Report (`report`, `scan`, `id_recluse`) VALUES ('" + relatorio + "', '" + scan + "', '" + identificacao1 + "');";
-                            Statement statement = connection.createStatement();
-                            statement.executeUpdate(query);
-                            System.out.println("inserido");
-                            msg = "Inserido com sucesso";
-                            // Toast.makeText(getApplicationContext(), "Criado com Sucesso!", Toast.LENGTH_SHORT);
-                            sucess = true;
-                            prenchido = true;
-                        } else {
-                            sucess = false;
-                            msg = "Identificação de Recluso inválida!";
-                            System.out.println("CONA");
-                            //  Toast.makeText(getApplicationContext(), "Erro ao identificar Recluso!", Toast.LENGTH_SHORT);
-                        }
+                    //if (prenchido == true) {
+                    String query1 = "SELECT COUNT(1) FROM Recluse WHERE id_recluse like '" + t_identific + "';";
+                    Statement statement1 = connection.createStatement();
+                    ResultSet resultSet1 = statement1.executeQuery(query1);
+                    while (resultSet1.next()) {
+                        valor = resultSet1.getInt("COUNT(1)");
                     }
+                    if (valor > 0 && valor < 2) {
+                        String query = "INSERT INTO Report (`report`, `scan`, `id_recluse`) VALUES ('" + t_relatorio + "', '" + scan + "', '" + t_identific + "');";
+                        Statement statement = connection.createStatement();
+                        statement.executeUpdate(query);
+                        msg = "Inserido com sucesso";
+                        sucess = true;
+                        // prenchido = true;
+                        System.out.println(sucess);
+                        //  System.out.println(prenchido);
+                    } else {
+                        sucess = false;
+                        msg = "Identificação de Recluso inválida!";
+                    }
+                    //   }
                 }
 
                 connection.close();
             } catch (Exception e){
                 msg = "Connection correu mal";
-                //Toast.makeText(getApplicationContext(), "Não foi possível comunicar com a BD!", Toast.LENGTH_SHORT);
                 sucess = false;
             }
             return msg;
