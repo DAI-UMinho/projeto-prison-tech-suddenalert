58d0bbfdbbb31458204e42d1dcb76fe2a4975a06
rafaeltarrendo41 <a89162@alunos.uminho.pt>
2020-04-14 23:12:21 +0100

    Revert "Merge branch 'master' of https://github.com/DAI-UMinho/projeto-prison-tech-suddenalert"
    This reverts commit 16c6866d4ff8c456d4543c3227d73a95fcf65d55, reversing
    changes made to 37b4a0dec224d63908df8af033e2af8d40b2428b.

--- front pc 11-04/src/frontend/Historico.java
+++ front pc 11-04/src/frontend/Historico.java
@@ -66,7 +66,6 @@ public class Historico extends javax.swing.JFrame implements Serializable {
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Sudden Alert");
         setBackground(new java.awt.Color(255, 255, 255));
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
