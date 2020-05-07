19f6f42e44bb40128f96d5604ba350c38a33a54c
rafaeltarrendo41 <a89162@alunos.uminho.pt>
2020-05-08 00:58:48 +0100

    pontos e horarios

--- front+back android/app/src/main/java/android/example/dai2/BD.java
+++ front+back android/app/src/main/java/android/example/dai2/BD.java
@@ -4,6 +4,9 @@ public class BD {
     private static final String BD_URL = "jdbc:mysql://193.136.11.180:3306/suddenalert";
     private static final String USER = "suddenalertuser";
     private static final String PASS = "Suddenalert.0";
+    //private static final String BD_URL = "jdbc:mysql://193.136.11.180:3306/SuddenAlert2020";
+    //private static final String USER = "suddenalertuser";
+    //private static final String PASS = "Suddenalert.0";
 
     public static String getBdUrl() {
         return BD_URL;
--- front+back android/app/src/main/java/android/example/dai2/MainActivity.java
+++ front+back android/app/src/main/java/android/example/dai2/MainActivity.java
@@ -34,7 +34,7 @@ import java.sql.Statement;
 public class MainActivity extends AppCompatActivity implements LocationListener {
     private Button btnScan;
     private Button btnGetLocation;
-    private TextView txLocation;
+    private TextView txLocation, txtScan;
     private final int GPS_REQUEST = 200;
     private LocationManager locationManager;
     private ImageView imageView, imageView2;
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     private ImageView imageView9;
     private ImageView imageView12;
     private ImageView imageView10;
+    private  String name;
 
 
 
@@ -69,16 +70,33 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 integrator.setPrompt("Scan Camara");
                 integrator.setCameraId(0);
                 integrator.initiateScan();
+                try {
+                    Thread.sleep(1000);
+                }
+                catch (Exception e){
+                    System.out.print("erro");
+                }
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION}, GPS_REQUEST);
+                } else {
+                    getLocation();
+                }
             }
 
 
         });
         btnGetLocation = findViewById(R.id.btnGetLocation);
         txLocation = findViewById(R.id.txLocation);
+        txtScan = findViewById(R.id.txScan);
         imageView = (ImageView) findViewById((R.id.imageView11));
         imageView2 = (ImageView) findViewById(R.id.imageView12);
 
-        btnGetLocation.setOnClickListener(new View.OnClickListener() {
+        /*btnGetLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
@@ -93,7 +111,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 }
 
             }
-        });
+        });*/
 
 
     }
@@ -107,17 +125,19 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 Login login = new Login();
                 login.execute();
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(1200);
                 }
                 catch (Exception e){
                     System.out.print("erro");
                 }
+
                 if (sucess == true) {
                     Toast.makeText(getApplicationContext(), "Utilizador encontrado!", Toast.LENGTH_SHORT).show();
                     btnGetLocation.setEnabled(true);
                     imageView.setImageResource(imagens[0]);
+                    txtScan.setText(name);
                 } else {
-                    Toast.makeText(getApplicationContext(), "Utilizador não encontrado!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Utilizador não encontrado!", Toast.LENGTH_SHORT).show();
                 }
             } else {
                 Toast.makeText(this,"Scan Cancelado", Toast.LENGTH_SHORT).show();
@@ -128,7 +148,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     }
 
     private void alert(String msg) {
-        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -220,11 +240,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                     Statement stmt = conn.createStatement();
                     ResultSet rs = stmt.executeQuery(query);
                     while (rs.next()) {
-                        String name = rs.getString("name");
+                        name = rs.getString("name");
                         coordenadas = rs.getString("location");
                         estatuto = rs.getInt("id_type");
-                        System.out.println(estatuto);
-                        //name = rs.getString("name");
                         String[] points = coordenadas.split("\\s*[,]\\s*");
                         latitudePris = points[0];
                         longitudePris = points[1];
@@ -308,11 +326,10 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 */
     public void entrar(View v) {
          if (sucess == true && isSucess==true) {
- System.out.println("aqui");
             if (estatuto == 3) {
                 startActivity(new Intent(this, inicio_diretor.class));
              }
-             if (estatuto == 2) { System.out.println("aquibem");
+             if (estatuto == 2) {
                 startActivity(new Intent(this, inicio_psicologo.class));
 
              }
